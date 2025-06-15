88d560de23b5c0103e33d92818fb6cfa40298e5e
LianggaRistiana <liangga4@gmail.com>
2025-06-16 01:48:36 +0800

    feat: add navbar with mode toggle button

---
+++ components/molecules/navbar.tsx
@@ -0,0 +1,12 @@
+import { ModeToggle } from "../atoms/mode-toggle";
+
+export default function Navbar() {
+    return (
+        <nav className="fixed top-0 w-full gap-4 z-50 flex items-center justify-between py-4 px-4 lg:px-12 xl:px-24 backdrop-blur-lg bg-background/80 border-b ">
+            <p  className="text-xl font-bold text-primary">
+                Imaginary
+            </p>
+            <ModeToggle />
+        </nav>
+    );
+}
