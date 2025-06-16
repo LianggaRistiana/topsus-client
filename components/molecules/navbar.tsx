1288e75b1d801da7ca4d7556ca20919365deb9b9
LianggaRistiana <liangga4@gmail.com>
2025-06-16 12:14:06 +0800

    refactor: make stepgroup reusable

--- components/molecules/navbar.tsx
+++ components/molecules/navbar.tsx
@@ -3,19 +3,29 @@ import StepButton from "../atoms/step-button";
 import StepGroup from "./step-group";
 
 type NavbarProps = {
-  title?: string;
+    title?: string;
 };
 
 export default function Navbar({ title = "Imaginary" }: NavbarProps) {
-  return (
-    <nav className="fixed top-0 w-full gap-4 z-50 flex items-center justify-between py-4 px-4 lg:px-12 xl:px-24 backdrop-blur-lg bg-background/80 border-b">
-      <StepGroup />
-      
-      <p className="absolute left-1/2 -translate-x-1/2 text-xl font-bold text-primary">
-        {title}
-      </p>
+    return (
+        <nav className="fixed top-0 w-full gap-4 z-50 flex items-center justify-between py-4 px-4 lg:px-12 xl:px-24 backdrop-blur-lg bg-background/80 border-b">
+            <StepGroup
+                currentStep={3}
+                focusStep={3}
+                steps={[
+                    { title: "Pilih Paket", href: "/step-1" },
+                    { title: "Isi Data", href: "/step-2" },
+                    { title: "Pembayaran", href: "/step-3" },
+                    { title: "Selesai", href: "/step-4" },
+                    { title: "Selesai", href: "/step-5" },
+                ]}
+            />
 
-      <ModeToggle />
-    </nav>
-  );
+            <p className="absolute left-1/2 -translate-x-1/2 text-xl font-bold text-primary">
+                {title}
+            </p>
+
+            <ModeToggle />
+        </nav>
+    );
 }
--- components/molecules/step-group.tsx
+++ components/molecules/step-group.tsx
@@ -1,18 +1,41 @@
+"use client"
 import StepBridge from "../atoms/step-bridge";
 import StepButton from "../atoms/step-button";
 
-export default function StepGroup() {
-    return (
-        <div className="flex relative justify-between w-fit items-center">
-            <StepButton isActive={false} isDone={true} >1</StepButton>
-            <StepBridge isDone={true} />
-            <StepButton isActive={true} isDone={true}>2</StepButton>
-            <StepBridge isDone={false} />
-            <StepButton isActive={false} isDone={false}>3</StepButton>
-            <StepBridge isDone={false} />
-            <StepButton isActive={false} isDone={false} >4</StepButton>
-            <StepBridge isDone={false} />
-            <StepButton isActive={false} isDone={false} >5</StepButton>
-        </div>
-    )
+type StepItem = {
+  title: string;
+  href: string;
+};
+
+type StepGroupProps = {
+  steps: StepItem[];
+  currentStep: number; 
+  focusStep: number; 
+};
+
+
+export default function StepGroup({ steps, currentStep, focusStep }: StepGroupProps) {
+  return (
+    <div className="flex relative justify-between w-fit items-center">
+      {steps.map((step, index) => {
+        const stepNumber = index + 1;
+        // const isActive = stepNumber === currentStep;
+        const isActive = stepNumber === focusStep;
+        const isDone = stepNumber < currentStep;
+
+        return (
+          <div key={step.href} className="flex items-center">
+            <StepButton isActive={isActive} isDone={isDone} onClick={() => {
+              window.location.href = step.href;
+            }}>
+              {stepNumber}
+            </StepButton>
+            {index !== steps.length - 1 && (
+              <StepBridge isDone={stepNumber < currentStep - 1} />
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
